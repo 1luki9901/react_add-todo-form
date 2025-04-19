3cad546f9c60b4f97eb82b7c7bcc08a85890c4e5
Łukasz <1luki9901@gmail.com>
2025-04-19 18:21:37 +0200

    Solution

---
+++ src/components/TodoForm/TodoForm.tsx
@@ -0,0 +1,93 @@
+import { useState } from 'react';
+import { getAllUsers, getUserById } from '../../service/UserService';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  onSubmit: (todo: Todo) => void;
+};
+
+export const TodoForm: React.FC<Props> = ({ onSubmit }) => {
+  const [title, setTitle] = useState('');
+  const [hasTitleError, setTitleError] = useState(false);
+
+  const [userId, setUserId] = useState(0);
+  const [hasSelectError, setSelectError] = useState(false);
+
+  const resetForm = () => {
+    setTitle('');
+    setUserId(0);
+  };
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+    setTitleError(false);
+  };
+
+  const handleUserChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setUserId(+event.target.value);
+    setSelectError(false);
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    setTitleError(!title);
+    setSelectError(!userId);
+
+    if (!title || !userId) {
+      return;
+    }
+
+    onSubmit({
+      id: 0,
+      title,
+      completed: false,
+      user: getUserById(userId),
+      userId,
+    });
+
+    resetForm();
+  };
+
+  return (
+    <form action="/api/todos" method="POST" onSubmit={handleSubmit}>
+      <div className="field">
+        <label htmlFor="">
+          Title:&nbsp;
+          <input
+            type="text"
+            data-cy="titleInput"
+            value={title}
+            placeholder="Enter a title"
+            onChange={handleTitleChange}
+          />
+        </label>
+        {hasTitleError && <span className="error">Please enter a title</span>}
+      </div>
+
+      <div className="field">
+        <label htmlFor="">
+          User:&nbsp;
+          <select
+            data-cy="userSelect"
+            value={userId}
+            onChange={handleUserChange}
+          >
+            <option value="0">Choose a user</option>
+
+            {getAllUsers().map(user => (
+              <option key={user.id} value={user.id}>
+                {user.name}
+              </option>
+            ))}
+          </select>
+        </label>
+        {hasSelectError && <span className="error">Please choose a user</span>}
+      </div>
+
+      <button type="submit" data-cy="submitButton">
+        Add
+      </button>
+    </form>
+  );
+};
